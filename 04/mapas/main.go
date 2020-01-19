985a4cbd1bbd38d4486d0ea1da86b8aaa9b4c551
Bryan Agusto Cruz Castro <bryan.cc.652@gmail.com>
2020-01-19 15:41:33 -0400

    aprendiendo el manejo de mapas en Go land

---
+++ 04/mapas/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//M A P A S
+	// LLAVE
+	// TIPO DE VALOR
+	
+//
+/*
+idiomas:= map[string]string{
+	"es":"Español",
+	"en":"Ingles",
+	"fr":"Frances",
+	"ar":"argentino",
+
+*/
+
+
+	// idiomas := make(map[string]string)
+	// idiomas["es"] = "Español"
+	// idiomas["en"] = "Ingles"
+	// idiomas["fr"] = "Frances"
+	/*
+	fmt.Println(idiomas)
+
+	delete(idiomas,"en")
+
+	fmt.Println(idiomas)
+	
+	if idioma,ok := idiomas["en"];ok{
+		fmt.Println("la posicion si existe y vale", idioma)
+	}else{
+		fmt.Println("la posicion NO existe")
+	}
+	*/
+	//fmt.Println(idiomas["pt"])
+ numeros := map[uint8]string{
+	 1:"platano",
+	 200:"papaya",
+	 5:"chiromoya",
+ }
+
+	 fmt.Println(numeros)
+}
