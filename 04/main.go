4815bc4002b910f748933f423b3fcbb233b0a224
Bryan Agusto Cruz Castro <bryan.cc.652@gmail.com>
2020-01-19 11:30:10 -0400

    aprendiendo a manejar slice en go

--- 04/main.go
+++ 04/main.go
@@ -4,5 +4,15 @@ import (
 	"fmt"
 )
 func main (){
-	fmt.Println("arreglos")
+	//arrays
+	//todo los valores del mismo tipo
+	//tamaño fijo
+	/*var names [10]string
+	names[1] = "Bryan"
+	names[2]= "augusto"*/
+
+	names :=[3] string{"bryan","augusto","pedro"}
+	//name := names[1]
+	// array[inicio:final(excluyente)]
+	fmt.Println(names[1:2])
 }
